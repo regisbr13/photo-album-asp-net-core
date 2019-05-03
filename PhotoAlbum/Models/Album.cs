22bb2711bdb07c048f376cd0ab960d22cd867b6a
Regis Lima <regislima1391@gmail.com>
2019-05-03 15:15:42 -0300

    Definidas as entidades Album e Images

---
+++ PhotoAlbum/Models/Album.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PhotoAlbum.Models
+{
+    public class Album
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="campo obrigatório")]
+        [StringLength(50, ErrorMessage ="use até {1} caracteres")]
+        public string Name { get; set; }
+
+        public string Place { get; set; }
+
+        [Required(ErrorMessage ="campo obrigatório")]
+        public string Cover { get; set; }
+
+        [Required(ErrorMessage = "campo obrigatório")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString ="{0:dd/MM/yyyy}")]
+        public DateTime Data { get; set; }
+    }
+}
---
+++ PhotoAlbum/Models/Images.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhotoAlbum.Models
+{
+    public class Images
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="campo obrigatório")]
+        public string Path { get; set; }
+
+        public int AlbumId { get; set; }
+
+        public Album Album { get; set; }
+    }
+}
